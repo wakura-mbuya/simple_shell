2681606f783b76f9b048df7e13956fbcc21c0bd7
Nazavivian21 <nazafavourukwe@gmail.com>
2024-02-23 07:04:17 -0800

    Betty Checks

--- _strlen.c
+++ _strlen.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-/*
+/**
 *_strlen - Checks the length string
+*Description: This function should take  string and return it's length
 *@str: a string variable that stores strings
-*Return: 0 
+*Return: 0
 */
 int _strlen(char *str)
 {
-    int i;
-    int len;
-    len = 0;
-    for (i = 0;  str[i] != '\0'; i++)
-    {
-        len = len + 1;
-    }
-    printf("\n");
-    return (len);
+	int i;
+	int len;
+
+	len = 0;
+	for (i = 0;  str[i] != '\0'; i++)
+	{
+		len = len + 1;
+	}
+	printf("\n");
+	return (len);
 }
